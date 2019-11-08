62b2b16bef7e4e793730f5dabe5e2d599ea9dcc1
stewiegreen <53995844+stewiegreen@users.noreply.github.com>
2019-11-08 13:24:56 +1100

    cleaned up the code

--- organizer.py
+++ organizer.py
@@ -4,56 +4,51 @@ import shutil
 from PIL import Image
 import time
 
-
 class PhotoOrganizer:
     def __init__(self, path, move_to_path):
         self.path = path
         self.move_to_path = move_to_path
 
 #checks exif data
-    def check_exif(self):
-        file = []
-        for f in os.listdir(self.path):
-            if f.lower().endswith('.jpg'):
-                img = Image.open(self.path + f)
-                info = img._getexif()
-                if info:#if there is exif data continue
-                    if 36867 in info:
-                        date = info[36867]
-                        get_date = time.strptime(date, "%Y:%m:%d %H:%M:%S")
-                        year = get_date[0]
-                        month = get_date[1]
-                        file.append([self.path, f, year, month])
-        return file
-
-
-    def move_photos(self, file):
-        #parsing the list made in check_exif
-        for f in file:
-            p = f[0]
-            filename = f[1]
-            y = f[2]
-            m = f[3]
-            # if the path doesn't exist make the new directories and move file
-            if os.path.isdir(self.move_to_path + str(y) + "/" + str(m) + "/") == False:
-                os.makedirs(self.move_to_path + str(y) + "/" + str(m) + "/")
-                shutil.move(p + filename, self.move_to_path + str(y) + "/" + str(m) + "/")
-            #if the file already exists in the directory print a warning and skip file
-            elif os.path.isfile(self.move_to_path + str(y) + "/" + str(m) + "/" + filename) == True:
-                print(filename + " already exists in this directory")
-                pass
-            #all is good, file is being moved
+    def check_exif(self, f):
+        if f.lower().endswith('.jpg'):
+            img = Image.open(self.path + f)
+            info = img._getexif()
+            if info != None:#if there is exif data continue
+                if 36867 in info:
+                    date = info[36867]
+                    get_date = time.strptime(date, "%Y:%m:%d %H:%M:%S")
+                    year = get_date[0]
+                    month = get_date[1]
+                    return self.path, f, year, month
+                else:
+                    pass
             else:
-                shutil.move(p + filename, self.move_to_path + str(y) + "/" + str(m) + "/")
+                pass
+        else:
+            pass
 
 
+    def move_photos(self, path, filename, year, month):
+        # if the path doesn't exist make the new directories and move file
+        if os.path.isdir(self.move_to_path + str(y) + "/" + str(m) + "/") == False:
+            os.makedirs(self.move_to_path + str(y) + "/" + str(m) + "/")
+            shutil.move(p + filename, self.move_to_path + str(y) + "/" + str(m) + "/")
+        #if the file already exists in the directory print a warning and skip file
+        elif os.path.isfile(self.move_to_path + str(y) + "/" + str(m) + "/" + filename) == True:
+            print(filename + " already exists in this directory")
+            pass
+        #all is good, file is being moved
+        else:
+            shutil.move(p + filename, self.move_to_path + str(y) + "/" + str(m) + "/")
 
 
+path = input('Enter Directory path to Search (this will NOT search subfolders!): ')
+to_path = input('Enter Directory path to move photos to: ')
 
-folder = input('Enter Directory path to Search (this will NOT search subfolders!): ')
-to_folder = input('Enter Directory path to move photos to: ')
+photo = PhotoOrganizer(path, to_path)
 
-
-photo = PhotoOrganizer(folder, to_folder)
-
-photo.move_photos(photo.check_exif())
+for f in os.listdir(path):
+    if photo.check_exif(f) != None:
+        p, filename, y, m = photo.check_exif(f)
+        photo.move_photos(p, filename, y, m)
