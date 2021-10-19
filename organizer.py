739ff4d057aa50f5b579aa24e66e378048ea5320
stewiegreen <53995844+stewiegreen@users.noreply.github.com>
2021-10-19 23:39:07 +1100

    fixes

--- organizer.py
+++ organizer.py
@@ -12,7 +12,7 @@ class PhotoOrganizer:
 #checks exif data
     def check_exif(self, f):
         if f.lower().endswith('.jpg'):
-            img = Image.open(self.path + f)
+            img = Image.open(self.path + "/" + f)
             info = img._getexif()
             if info != None:#if there is exif data continue
                 if 36867 in info:
@@ -31,16 +31,16 @@ class PhotoOrganizer:
 
     def move_photos(self, path, filename, year, month):
         # if the path doesn't exist make the new directories and move file
-        if os.path.isdir(self.move_to_path + str(year) + "/" + str(month) + "/") == False:
-            os.makedirs(self.move_to_path + str(year) + "/" + str(month) + "/")
-            shutil.move(path + filename, self.move_to_path + str(year) + "/" + str(month) + "/")
+        if os.path.isdir(self.move_to_path + "/" + str(year) + "/" + str(month) + "/") == False:
+            os.makedirs(self.move_to_path + "/" + str(year) + "/" + str(month) + "/")
+            shutil.move(path + "/" + filename, self.move_to_path + "/" + str(year) + "/" + str(month) + "/")
         #if the file already exists in the directory print a warning and skip file
-        elif os.path.isfile(self.move_to_path + str(year) + "/" + str(month) + "/" + filename) == True:
+        elif os.path.isfile(self.move_to_path + "/" + str(year) + "/" + str(month) + "/" + filename) == True:
             print(filename + " already exists in this directory")
             pass
         #all is good, file is being moved
         else:
-            shutil.move(path + filename, self.move_to_path + str(year) + "/" + str(month) + "/")
+            shutil.move(path + "/" + filename, self.move_to_path + "/" + str(year) + "/" + str(month) + "/")
 
 def choose_destination():
     to_path = input('Enter Directory path to move photos to: ')
@@ -84,5 +84,6 @@ def main():
         if photo.check_exif(f) != None:
             p, filename, y, m = photo.check_exif(f)
             photo.move_photos(p, filename, y, m)
+            
 
 main()
